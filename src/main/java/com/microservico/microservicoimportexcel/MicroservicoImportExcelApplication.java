811c9d2468e5199d92fc53d29f202b283a12f78b
aramos/fazenda <aramos@fazenda.ms.gov.br>
2019-11-05 13:43:55 -0400

    inicialiando projeto

---
+++ src/main/java/com/microservico/microservicoimportexcel/MicroservicoImportExcelApplication.java
@@ -0,0 +1,13 @@
+package com.microservico.microservicoimportexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicoImportExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicoImportExcelApplication.class, args);
+	}
+
+}
